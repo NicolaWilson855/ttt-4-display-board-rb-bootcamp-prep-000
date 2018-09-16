87b4a132e9117460a7a6d63cf7ba9bd7d82ac5a8
nicola.wilson855 <nicola.wilson855@gmail.com>
2018-09-16 21:29:46 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,8 +1,6 @@
 def display_board(board)
   board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
-  row_1 = [" ", " ", " "]
-  row_2 = [" ", "x", " "]
-  row_3 = [" ", " ", " "]
+
   puts "   |   |   "
   puts "-----------"
   puts "   |   |   "
